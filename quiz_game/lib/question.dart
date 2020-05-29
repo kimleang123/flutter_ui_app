a7f78f36880d1b4a4b8d6c70126aaeadf95037a4
kimleang <lykimleang456@gmail.com>
2020-05-29 07:38:02 +0700

    quiz game

--- quiz_game/lib/question.dart
+++ quiz_game/lib/question.dart
@@ -2,8 +2,10 @@ class Question{
   String textQuestion;
   bool checkAnswer;
   //create a contractor of class Question
-  Question({String s, bool b}){
-    textQuestion=s;
-    checkAnswer=b;
-  }
+  // Question({String s, bool b}){
+  //   textQuestion=s;
+  //   checkAnswer=b;
+  // }
+  //the same like code above
+  Question({this.textQuestion,this.checkAnswer});
 }
